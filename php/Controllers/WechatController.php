baece33bfd4b87077df1b5d75be98e64e76a9119
boxshadow <idafoo@sina.com>
2017-11-05 01:43:33 +0800

    b

--- php/Controllers/WechatController.php
+++ php/Controllers/WechatController.php
@@ -2,9 +2,9 @@
    
 namespace Zhiyi\Component\ZhiyiPlus\PlusComponentH5\Controllers;
 
-use Illuminate\Http\Request;
-
 use Log;
+use Illuminate\Http\Request;
+use Zhiyi\Plus\Http\Controllers\Controller;
 
 class WechatController extends Controller
 {
