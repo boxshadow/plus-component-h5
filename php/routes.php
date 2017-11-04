4d9f605a40a1fb5594c00ddf558d9e4b837a29ca
boxshadow <idafoo@sina.com>
2017-11-05 01:26:47 +0800

    b

--- php/routes.php
+++ php/routes.php
@@ -8,10 +8,12 @@ Route::namespace('Zhiyi\\Component\\ZhiyiPlus\\PlusComponentH5\\Controllers')
         	->where('route', '.*')
             ->name('H5');
     });
-Route::namespace('Zhiyi\\Component\\ZhiyiPlus\\PlusComponentH5\\Controllers')
-    ->any('/wechat', function () {
-        return recirect()->route('H5');
+Route::group(['middleware' => ['web', 'wechat.oauth']], function () {
+    Route::get('/wechat', function () {
+        
+        return redirect()->route('H5');
     });
+});
 Route::namespace('Zhiyi\\Component\\ZhiyiPlus\\PlusComponentH5\\Controllers')
     ->prefix('/h5')
     // ->middleware('h5')
